d6ad7dd359dc82d1c3f662dc4efbb7b6fdff0ad6
muhamad fajar <muhamadfajar015@gmail.com>
2021-11-14 19:12:17 +0700

    first commit

--- src/test/java/starter/account/login/LoginSteps.java
+++ src/test/java/starter/account/login/LoginSteps.java
@@ -1,4 +1,16 @@
 package starter.account.login;
 
+
+import net.thucydides.core.annotations.Steps;
+
 public class LoginSteps {
+
+    @Steps
+    Login login;
+
+
+
+
+
+
 }
--- src/test/java/starter/utils/Endpoint.java
+++ src/test/java/starter/utils/Endpoint.java
@@ -1,60 +1,10 @@
 package starter.utils;
 
-import io.restassured.RestAssured;
-import net.serenitybdd.rest.SerenityRest;
-import net.thucydides.core.annotations.Step;
-import org.json.simple.JSONObject;
-
-import java.net.URI;
-import java.net.URL;
-
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-
 public class Endpoint {
 
+    public String BASE_URL = "https:// demoga.com";
+    public String  LOGIN = BASE_URL+ "/account/v1/Login";
 
 
-    Endpoint endpoint = new Endpoint();
-    JSONObject requestParams;
-    private URL LOGIN;
-
-    @Step
-    public void setBodyReq(){
-        requestParams = new JSONObject();
-        requestParams.put("userName","");
-        requestParams.put("password","");
-    }
-
-    @Step
-    public void hitEndpoindLogin(){
-        SerenityRest
-                .given()
-                .header("Content-Type","application/json")
-                .body(requestParams.toString())
-                .when()
-                .post(endpoint.LOGIN)
-                .then()
-                .statusCode(200);
-    }
-
-    @Step
-    public void hitEndpoindLoginRA(){
-        RestAssured
-                .given()
-                .header("Content-Type","application/json")
-                .body(requestParams.toString())
-                .when()
-                .post(endpoint.LOGIN)
-                .then()
-                .statusCode(200);
-    }
-
-    @Step
-    public void valdateEndpointLogin(){
-        SerenityRest
-                .then()
-                .body(matchesJsonSchemaInClasspath("JSONSchema/login.json"));
-
-    }
 
 }
